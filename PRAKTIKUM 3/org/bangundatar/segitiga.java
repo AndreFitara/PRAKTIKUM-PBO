b715de003ca8b41dbd9f849c5861ba808f4ef8f6
AndreFitara <100498056+AndreFitara@users.noreply.github.com>
2023-03-08 19:39:34 +0700

    Add files via upload

---
+++ PRAKTIKUM 3/org/bangundatar/segitiga.java
@@ -0,0 +1,28 @@
+/**
+* Segitiga.java 08/03/23
+* Penulis : Andre Fitara Agun Ni'am Muta'ali
+* Deskripsi : representasi dari objek segitiga, turunan kelas poligon
+*
+*/
+
+package org.bangundatar;
+
+import org.poligon.Poligon;
+
+public class Segitiga extends Poligon{
+	private double alas, tinggi;
+	
+	public Segitiga(double alas, double tinggi, int jumlahSisi){
+		this.alas = alas;
+		this.tinggi = tinggi;
+		this.jumlahSisi = jumlahSisi;
+	}
+	
+	public double hitungLuas(){
+		return alas * tinggi / 2;
+	}
+	
+	public void printInfo(){
+		System.out.println("Bangun Segitiga bersisi " + this.getJumlahSisi());
+	}
+}
