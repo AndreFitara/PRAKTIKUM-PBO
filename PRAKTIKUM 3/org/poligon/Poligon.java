d966845fb5ed8bfc4a82ebfa13cb84e512149f2c
AndreFitara <100498056+AndreFitara@users.noreply.github.com>
2023-03-08 17:22:02 +0700

    Add files via upload

---
+++ PRAKTIKUM 3/org/poligon/Poligon.java
@@ -0,0 +1,16 @@
+/**
+* Poligon.java 08/03/23
+* Penulis : Andre Fitara Agun Ni'am Muta'ali
+* Deskripsi : representasi dari objek persegi panjang, turunan kelas poligon
+*
+*/
+
+package org.poligon;
+
+public class Poligon{
+	protected int jumlahSisi;
+	
+	public int getJumlahSisi(){
+		return this.jumlahSisi;
+	}
+}
