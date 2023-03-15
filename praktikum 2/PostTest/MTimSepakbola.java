d5292a49e9244ab9f71a5eb1005e3201f821f576
AndreFitara <100498056+AndreFitara@users.noreply.github.com>
2023-03-15 16:51:48 +0700

    Update MTimSepakbola.java

--- praktikum 2/PostTest/MTimSepakbola.java
+++ praktikum 2/PostTest/MTimSepakbola.java
@@ -4,15 +4,17 @@
 * NIM :24060121140129
 */
 
+
 class MTimSepakbola {
   public static void main(String[] args) {
     // TODO: Make an instance called 'liverpool' -> use constructor which you have made before
-    
+    TimSepakbola liverpool = new TimSepakbola("Liverpool");
     // TODO: Make an instance called 'manchesterUnited' -> use constructor which you have made before
-    
+    TimSepakbola manchesterUnited = new TimSepakbola("Manchaster United");
 
     // TODO: call startTandingBola()
-		
+    liverpool.startTandingBola(manchesterUnited);
+
 
 		// Assume that liverpool wins against manchesterUnited 7 - 0
     for (int i = 0; i < 7; i++) {
@@ -20,15 +22,17 @@ class MTimSepakbola {
 
 			// TODO: Replace [SKOR LIVERPOOL] and [SKOR MANCHESTERUNITED] with the correct METHOD
 			// Code below should produce the output "Skor sementara: Liverpool 7 - 0 Manchester United"
-			System.out.println("Skor sementara: " + liverpool.nama + " " + [SKOR LIVERPOOL] + " - " + [SKOR MANCHESTERUNITED] + " " + manchesterUnited.nama);
+			System.out.println("Skor sementara: " + liverpool.nama + " " + liverpool.getSkor() + " - " + manchesterUnited.getSkor() + " " + manchesterUnited.nama);
     }
 
+	  
 		// TODO: call endTandingBola()
-		
+    manchesterUnited.endTandingBola(liverpool);
   }
 }
 
 /* 
+
 Expected output:
 
 Pertandingan antara Liverpool dan Manchester United dimulai!
