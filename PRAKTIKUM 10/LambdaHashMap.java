fc921db58a2239d8c08e84c34022c9c66e58d255
AndreFitara <100498056+AndreFitara@users.noreply.github.com>
2023-06-06 23:41:03 +0700

    Update LambdaHashMap.java

--- PRAKTIKUM 10/LambdaHashMap.java
+++ PRAKTIKUM 10/LambdaHashMap.java
@@ -5,26 +5,14 @@
 
 import java.util.*;
 
-public class MapTest{
-	public static void main(String[] args){
-		//kunci -> integer, nilai -> string
-		Map<Integer,String> map = new HashMap<Integer,String>();
-		//menempatkan elemen kunci dan nilai
-		map.put(1,"Satu");
-		map.put(2,"Dua");
-		//mengambil elemen pertama
-		System.out.println(map.get(1));
-		//mengambil keseluruhan kunci sebagai objek collection Set
-		Set<Integer> key = map.keySet();
-		//bagaimana iterasi untuk mengambil keseluruhan
-		//nilai dari kunci? Tulis pada laporan anda!
-		//petunjuk : gunakan iterasi seperti program ArrayListTest
-		
-		//jawab:
-		//menggunakan for untuk mendefinisikan k sebagai elemen dari key, kemudian melakukan pencetakan ke layar dengan menggunakan k itu sendiri
-        //untuk mendapatkan value dari k, dapat menggunakan operator get()
-        for (Integer k : key) {
-            System.out.println(map.get(k));
-        }
-	}
+public class LambdaHashMap {
+    public static void main(String[] args) {
+        Map<String, String> mahasiswaMap = new HashMap<>();
+        mahasiswaMap.put("24060121120029", "Joko");
+        mahasiswaMap.put("24060121130033", "Bara");
+        mahasiswaMap.put("24060121140123", "Dodi");
+        mahasiswaMap.put("24060121130456", "Nia");
+
+        mahasiswaMap.forEach((nim, nama) -> System.out.println("NIM: " + nim + " Nama: " + nama ));
+    }
 }
